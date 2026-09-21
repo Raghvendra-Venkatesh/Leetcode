e6f99ec52cc9255e38e4deba63d9270d45829b9e
Raghvendra Venkatesh <ragtag2509@gmail.com>
2026-09-21 22:36:43 +0530

    Time: 7 ms (7.95%) | Memory: 8.9 MB (21.76%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.c
@@ -0,0 +1,17 @@
+bool isPalindrome(int x) {
+    int n=x;
+    long int n1=0;
+    if(n>0){
+    while(n>0){
+        int d=n%10;
+        n1=n1*10+d;
+        n/=10;
+    }
+    }
+    if(x==n1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
