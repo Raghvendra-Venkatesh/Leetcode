9cde4bbf20f18b28ab075b77fadada7808ed22b5
Raghvendra Venkatesh <ragtag2509@gmail.com>
2026-09-24 19:25:44 +0530

    Time: 3 ms (61.22%) | Memory: 10.5 MB (39.30%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    bool issafe(vector<string> &board, int row, int col, int n){
+        for(int i=0;i<row;i++){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    void solve(vector<string> &board,int r,int n){
+        if(r==n){
+            ans.push_back(board);
+            return;
+        }
+        for(int col=0;col<n;col++){
+            if(issafe(board,r,col,n)){
+                board[r][col]='Q';
+                solve(board,r+1,n);
+                board[r][col]='.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+       vector<string> board(n,string(n,'.'));
+       solve(board,0,n);
+       return ans;
+    }
+};
